63f20fb6c5f79ecab39cbb692de622b967463c6e
Andrey <andreywtf@mail.ru>
2018-12-17 07:50:08 +0400

    Урок 3, без делегата

---
+++ BrandNewShip/HealthPack.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace BrandNewShip
+{
+    class HealthPack: Asteroid
+    {        
+        public HealthPack(Point pos, Point dir, Size size) : base(pos, dir, size)
+        {           
+        }
+
+        public override void Draw()
+        {
+            Game.buffer.Graphics.FillEllipse(Brushes.Green, Pos.X, Pos.Y, Size.Width, Size.Height);
+        }
+
+        public override void Update()
+        {
+            Pos.X = Pos.X + Dir.X;
+            if (Pos.X < -Size.Width) Pos.X = Game.Width + Size.Width;            
+        }
+    }
+}
---
+++ BrandNewShip/Star.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace BrandNewShip
+{
+    class Star : BaseObject
+    {
+        public Star(Point pos, Point dir, Size size) : base(pos, dir, size)
+        {            
+        }
+
+        public override void Draw()
+        {
+            Game.buffer.Graphics.DrawLine(Pens.White, Pos.X - Size.Width, Pos.Y + Size.Height, Pos.X + Size.Width, Pos.Y - Size.Height);
+            Game.buffer.Graphics.DrawLine(Pens.White, Pos.X + Size.Width, Pos.Y + Size.Height, Pos.X - Size.Width, Pos.Y - Size.Height);
+        }
+
+        public override void Update()
+        {           
+            Pos.X = Pos.X + Dir.X;
+            if (Pos.X < 0) Pos.X = Game.Width;
+            if (Pos.X > Game.Width) Dir.X = -Dir.X;
+        }
+    }
+}
+
